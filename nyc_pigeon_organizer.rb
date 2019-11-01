87c629df60d61a9e7a793e7beb6b61774270b31f
Melike KILIC <melike180400@gmail.com>
2019-11-01 18:49:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_list = {}
+ data.each do |property,property_names|
+  property_names.each do |property_values, pigeon_names|
+    pigeon_names.each do |name|
+     pigeon_list[name] ||= {}
+     pigeon_list[name][property] ||=[]
+     pigeon_list[name][property].push(property_values.to_s)
+      end
+   end
+ end
+ pigeon_list
 end
