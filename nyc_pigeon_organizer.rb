6ef0ef5489aca9ed77458f5b9d17854c3603ef58
Melike KILIC <melike180400@gmail.com>
2019-11-01 18:18:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,12 @@
   def nyc_pigeon_organizer(data)
   pigeon_list = {}
 
-  data.each do |attribute_name, attributes|
-    attributes.each do |attribute_value, pigeon_names|
+  data.each do |keys, values|
+    values.each do |attribute_value, pigeon_names|
       pigeon_names.each do |name|
         pigeon_list[name] ||= {}
-        pigeon_list[name][attribute_name] ||= []
-        pigeon_list[name][attribute_name].push(attribute_value.to_s)
+        pigeon_list[name][keys] ||= []
+        pigeon_list[name][keys].push(attribute_value.to_s)
       end
     end
   end
